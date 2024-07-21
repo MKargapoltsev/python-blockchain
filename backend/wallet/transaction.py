e7266b0e745ac9b3724d4b7dc0a1ab0b1022291a
MKargapoltsev <36708496+MKargapoltsev@users.noreply.github.com>
2024-07-21 14:33:56 +0200

    Add transaction chain validation

--- backend/wallet/transaction.py
+++ backend/wallet/transaction.py
@@ -89,6 +89,11 @@ class Transaction:
         Validate a transaction.
         Raise an exception for invaled transaction.
         """
+        if transaction.input == MINING_REWARD_INPUT:
+            if list(transaction.output.values()) != [MINING_REWARD]:
+                raise Exception('Invalid mining reward')
+            return
+        
         output_total = sum(transaction.output.values())
 
         if transaction.input['amount'] != output_total:
